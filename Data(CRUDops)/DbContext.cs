af8560c2f2100cf347e8f462df365764c49856fe
Abbas Mohammed <abbas.mohammed@trojans.dsu.edu>
2024-07-28 16:29:37 -0400

    Features

--- Data(CRUDops)/DbContext.cs
+++ Data(CRUDops)/DbContext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using AviationMaintenanceManagementSystem.ModelClasses;
+using AviationMaintenanceManagementSystem.Features;
 
 namespace AviationMaintenanceManagementSystem.Data_CRUDops_
 {
---
+++ Features/Wo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AviationMaintenanceManagementSystem.ModelClasses;
+
+namespace AviationMaintenanceManagementSystem.Features
+{
+    public interface IWorkOrderFeature
+    {
+        IEnumerable<BasicWorkOrder> GetAllWorkOrders();
+        BasicWorkOrder GetWorkOrderById(int id);
+        void CreateWorkOrder(BasicWorkOrder workOrder);
+        void UpdateWorkOrder(BasicWorkOrder workOrder);
+        void DeleteWorkOrder(int id);
+        void ProcessWorkOrder(int id);
+    }
+}

30a9356237d4dbf61d02dddd48df64435d91aa40
Abbas Mohammed <abbas.mohammed@trojans.dsu.edu>
2024-07-28 20:34:28 -0400

    forms

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace AviationMaintenanceManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MaintenanceDashboard());
         }
     }
 }

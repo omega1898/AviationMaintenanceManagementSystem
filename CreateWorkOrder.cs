9db0cc3cfb4d5d16a2f0f6399dec14c2c48ffc3f
Abbas Mohammed <abbas.mohammed@trojans.dsu.edu>
2024-07-29 16:31:45 -0400

    forms

--- CreateWorkOrder.cs
+++ CreateWorkOrder.cs
@@ -15,9 +15,31 @@ namespace AviationMaintenanceManagementSystem
 {
     public partial class CreateWorkOrder : Form
     {
-        public CreateWorkOrder()
+        private readonly IWorkOrderFeature _workOrderFeature;
+
+        public CreateWorkOrder(IWorkCenterFeature workOrderFeature)
         {
+            _workOrderFeature = workOrderFeature();
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var workOrder = new SpecialWorkOrder
+            {
+
+               JobNumber = Convert.ToInt32(txtJobNumber.Text),
+                WorkCenter = txtWorkCenter.Text,
+                WorkOrderType = txtWorkOrderType.Text,
+                WorkOrderStatus = txtWorkOrderStatus.Text,
+                WorkOrderStartDate = Convert.ToDateTime(txtWorkOrderStartDate.Text),
+                WorkOrderEndDate = Convert.ToDateTime(txtWorkOrderEndDate.Text),
+                WorkOrderPriority = txtWorkOrderPriority.Text,
+                WorkOrderDescription = txtWorkOrderDescription.Text,
+                WorkOrderNotes = txtWorkOrderNotes.Text,
+                WorkOrderLaborHours = Convert.ToInt32(txtWorkOrderLaborHours.Text),
+                WorkOrderLaborRate = Convert.ToDecimal(txtWorkOrderLaborRate.Text),
+                WorkOrderTotalCost = Convert.ToDecimal(txtWorkOrderTotalCost}
         }
     }
 }

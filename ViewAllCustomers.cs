a7e6d7c9cbbc7eca419b9644852a996edeb919fd
Abbas Mohammed <abbas.mohammed@trojans.dsu.edu>
2024-07-31 20:15:12 -0400

    customer forms

---
+++ ViewAllCustomers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AviationMaintenanceManagementSystem.Features;
+using AviationMaintenanceManagementSystem.ModelClasses;
+
+
+namespace AviationMaintenanceManagementSystem
+{
+    public partial class ViewAllCustomers : Form
+    {
+        private readonly ICustomerFeature _customerFeature;
+
+        public ViewAllCustomers(ICustomerFeature customerFeature)
+        {
+            _customerFeature = customerFeature;
+            InitializeComponent();
+        }
+
+
+        private void ViewAllCustomers_Load(object sender, EventArgs e)
+        {
+            var customers = _customerFeature.GetAllCustomers();
+            dataGridView1.DataSource = customers;
+        }
+
+    }
+}

adf60eab7dabf0b27f67263b51442e1b88aa3d60
Abbas Mohammed <abbas.mohammed@trojans.dsu.edu>
2024-07-31 21:33:13 -0400

    inventory

--- AddInventory.Designer.cs
+++ AddInventory.Designer.cs
@@ -28,20 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtPartNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomenclature = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtManufacturer = new System.Windows.Forms.TextBox();
+            this.txtDelivery = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtAssignedJobNumber = new System.Windows.Forms.TextBox();
+            this.txtWorkCenter = new System.Windows.Forms.TextBox();
+            this.txtReceivedBy = new System.Windows.Forms.TextBox();
+            this.txtDateReceived = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(186, 364);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(111, 47);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(454, 364);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(111, 47);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtPartNumber
+            // 
+            this.txtPartNumber.Location = new System.Drawing.Point(16, 44);
+            this.txtPartNumber.Name = "txtPartNumber";
+            this.txtPartNumber.Size = new System.Drawing.Size(734, 26);
+            this.txtPartNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Part Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nomenclature";
+            // 
+            // txtNomenclature
+            // 
+            this.txtNomenclature.Location = new System.Drawing.Point(16, 106);
+            this.txtNomenclature.Name = "txtNomenclature";
+            this.txtNomenclature.Size = new System.Drawing.Size(734, 26);
+            this.txtNomenclature.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Work Center";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Job Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Manufacturer";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(388, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Delivery Status";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(192, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Price Per Unit";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Quantity";
+            // 
+            // txtManufacturer
+            // 
+            this.txtManufacturer.Location = new System.Drawing.Point(16, 228);
+            this.txtManufacturer.Name = "txtManufacturer";
+            this.txtManufacturer.Size = new System.Drawing.Size(734, 26);
+            this.txtManufacturer.TabIndex = 12;
+            // 
+            // txtDelivery
+            // 
+            this.txtDelivery.Location = new System.Drawing.Point(392, 167);
+            this.txtDelivery.Name = "txtDelivery";
+            this.txtDelivery.Size = new System.Drawing.Size(358, 26);
+            this.txtDelivery.TabIndex = 13;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(196, 167);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(171, 26);
+            this.txtPrice.TabIndex = 14;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(16, 167);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(151, 26);
+            this.txtQuantity.TabIndex = 15;
+            // 
+            // txtAssignedJobNumber
+            // 
+            this.txtAssignedJobNumber.Location = new System.Drawing.Point(16, 289);
+            this.txtAssignedJobNumber.Name = "txtAssignedJobNumber";
+            this.txtAssignedJobNumber.Size = new System.Drawing.Size(166, 26);
+            this.txtAssignedJobNumber.TabIndex = 16;
+            // 
+            // txtWorkCenter
+            // 
+            this.txtWorkCenter.Location = new System.Drawing.Point(196, 289);
+            this.txtWorkCenter.Name = "txtWorkCenter";
+            this.txtWorkCenter.Size = new System.Drawing.Size(180, 26);
+            this.txtWorkCenter.TabIndex = 17;
+            // 
+            // txtReceivedBy
+            // 
+            this.txtReceivedBy.Location = new System.Drawing.Point(578, 289);
+            this.txtReceivedBy.Name = "txtReceivedBy";
+            this.txtReceivedBy.Size = new System.Drawing.Size(172, 26);
+            this.txtReceivedBy.TabIndex = 18;
+            // 
+            // txtDateReceived
+            // 
+            this.txtDateReceived.Location = new System.Drawing.Point(392, 289);
+            this.txtDateReceived.Name = "txtDateReceived";
+            this.txtDateReceived.Size = new System.Drawing.Size(173, 26);
+            this.txtDateReceived.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(574, 266);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Received By";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(388, 266);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(114, 20);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Date Received";
             // 
             // AddInventory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(762, 745);
+            this.ClientSize = new System.Drawing.Size(762, 429);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtDateReceived);
+            this.Controls.Add(this.txtReceivedBy);
+            this.Controls.Add(this.txtWorkCenter);
+            this.Controls.Add(this.txtAssignedJobNumber);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtDelivery);
+            this.Controls.Add(this.txtManufacturer);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNomenclature);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPartNumber);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
             this.Name = "AddInventory";
             this.Text = "AddInventory";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtPartNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNomenclature;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtManufacturer;
+        private System.Windows.Forms.TextBox txtDelivery;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtAssignedJobNumber;
+        private System.Windows.Forms.TextBox txtWorkCenter;
+        private System.Windows.Forms.TextBox txtReceivedBy;
+        private System.Windows.Forms.TextBox txtDateReceived;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
--- AddInventory.cs
+++ AddInventory.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AviationMaintenanceManagementSystem.Features;
+using AviationMaintenanceManagementSystem.ModelClasses;
 
 namespace AviationMaintenanceManagementSystem
 {
     public partial class AddInventory : Form
     {
-        public AddInventory()
+        private readonly IInventoryFeature _inventoryFeature;
+
+        public AddInventory(IInventoryFeature inventoryFeature)
         {
+            _inventoryFeature = inventoryFeature;
             InitializeComponent();
+            btnCancel.Click += btnCancel_Click;
+            btnSave.Click += btnSave_Click;
+        }
+       
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtQuantity.Text, out int quantity) &&
+                decimal.TryParse(txtPrice.Text, out decimal pricPerUnit))
+            {
+                MessageBox.Show("Quantity must be an Integer");
+                return;
+            }
+            var inventory = new Inventory
+            {
+                PartNumber = txtPartNumber.Text,
+                Nomenclature = txtNomenclature.Text,
+                Quantity = Convert.ToInt32(txtQuantity.Text),
+                PricePerUnit = Convert.ToDecimal(txtPrice.Text),
+                Manufacturer = txtManufacturer.Text,
+                DeliveryStatus = txtDelivery.Text,
+                AssignedJobNumber = txtAssignedJobNumber.Text,
+                AssignedWorkCenter = txtWorkCenter.Text,
+                RecievedDate = Convert.ToDateTime(txtDateReceived.Text),
+                ReceivedBy = txtReceivedBy.Text
+            };
+
+            _inventoryFeature.CreateInventory(inventory);
+            MessageBox.Show("Inventory Added Successfully");
+            ClearForm();
+            this.Close();
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearForm()
+        {
+            txtPartNumber.Text = "";
+            txtNomenclature.Text = "";
+            txtQuantity.Text = "";
+            txtPrice.Text = "";
+            txtManufacturer.Text = "";
+            txtDelivery.Text = "";
+            txtAssignedJobNumber.Text = "";
+            txtWorkCenter.Text = "";
+            txtDateReceived.Text = "";
+            txtReceivedBy.Text = "";
         }
     }
 }
